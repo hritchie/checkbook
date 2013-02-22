95083905777daeeff169c0dd248be7288d32fff6
Heath Ritchie <heath@heathritchie.com>
2013-02-21 20:40:22 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkbook::Application.config.secret_token = 'bd8930244a5ee74a9e979e54d3fab190772ead876bd28c75da1f4896f6359fd5f872a2d4019a072516683f4b20dedf833b9848e2f8ef8a00848319094fd2f230'
