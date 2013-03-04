1d5f37c98dc40e89b6ba0bd38a1012f7dbbf70f6
Heath Ritchie <heath@heathritchie.com>
2013-03-04 13:50:30 -0500

    Set root route to checks#index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Checkbook::Application.routes.draw do
 
   resources :checks
 
+  root :to => 'checks#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,9 +51,6 @@ Checkbook::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
